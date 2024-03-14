e70ff0be2de6f6cdae924746e81ea38b05b0913f
RAIZEL <yureasel@gmail.com>
2024-03-14 23:43:59 +0800

    adding OpFlips

---
+++ EGO/Assets/Scripts/OpFlip.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpFlip : MonoBehaviour
+{
+        private SpriteRenderer sprite;
+    private void UpdateAnimation(){
+        if (CompareTag("blueOp")){
+
+        }
+    }
+
+    
+}
