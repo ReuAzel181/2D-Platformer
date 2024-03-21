2cb1055431d5f94892b74d902a016006f925848e
RAIZEL <yureasel@gmail.com>
2024-03-21 21:21:57 +0800

    adding OpFlips

--- EGO/Assets/Scripts/OpFlip.cs
+++ EGO/Assets/Scripts/OpFlip.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class OpFlip : MonoBehaviour
 {
-        private SpriteRenderer sprite;
-    private void UpdateAnimation(){
-        if (CompareTag("blueOp")){
+    private SpriteRenderer sprite;
+    private int currentIndex = 0;
+    [SerializeField] private Transform[] flipPoints;
+    [SerializeField] private float speed = 2f;
 
-        }
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    
+    private void Update()
+    {
+
+
+        Vector3 direction = flipPoints[currentIndex].position - transform.position;
+        sprite.flipX = (direction.x < 0);
+
+        if (Vector2.Distance(transform.position, flipPoints[currentIndex].position) < 0.1f)
+        {
+            currentIndex = (currentIndex + 1) % flipPoints.Length;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, flipPoints[currentIndex].position, Time.deltaTime * speed);
+    }
 }
