f5eb72d629f15153ec129b7dcf29eb044dc89177
RAIZEL <yureasel@gmail.com>
2024-03-21 21:22:21 +0800

    adding PlayerDeath code

--- EGO/Assets/Scripts/PlayerDeath.cs
+++ EGO/Assets/Scripts/PlayerDeath.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Player_Death : MonoBehaviour
 {
+    [SerializeField] private AudioSource deathSoundEffect;
     private Rigidbody2D rb;
     private Animator anim;
 
@@ -15,9 +16,11 @@ public class Player_Death : MonoBehaviour
  
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.CompareTag("blueOp")){
+            deathSoundEffect.Play();
             Die();
         }
         if (collision.gameObject.CompareTag("Spike")){
+            deathSoundEffect.Play();
             Die();
         }
     }
